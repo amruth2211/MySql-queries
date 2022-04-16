a39654066449370a26306147ab9bbc3bff3bd287
amruth2211 <chowdaryamu99@gmail.com>
2022-04-16 13:13:23 +0530

    Bank Project

--- BankingProject.sql
+++ BankingProject.sql
@@ -3,35 +3,37 @@ DROP DATABASE IF EXISTS `BankingProject`;
 -- create a database
 CREATE DATABASE IF NOT EXISTS `BankingProject`;
 
-USE `BankingProject`;
+USE `bankingproject`;
 
 DROP TABLE IF EXISTS `customers`;
 
 CREATE TABLE `customers` (
   `customerId` int(11) NOT NULL,
     name VARCHAR(255) ,
-    address VARCHAR(255) ,
-   PRIMARY KEY (`customerId`)
+    address INT NOT NULL,
+   PRIMARY KEY (`customerId`),
+    FOREIGN KEY (`ADDRESS`) REFERENCES ADDRESS (ADDRESSID)
 ) ;
 
 
 CREATE table banks(
     Bankcode INTEGER NOT NULL,
     BankName VARCHAR(255) NOT NULL,
-    MainOfficeAddress VARCHAR(255) ,
-    Primary Key (Bankcode)
+    MainOfficeAddress INT ,
+    Primary Key (Bankcode),
+    FOREIGN KEY (`MAINOFFICEADDRESS`) REFERENCES ADDRESS (ADDRESSID)
 );
 
 
 CREATE TABLE BRANCHES (
     BranchCode INTEGER NOT NULL,
-    Address VARCHAR(255) ,
+    Address INT ,
     Manager  VARCHAR(255) ,
     HeadOffice VARCHAR(255) ,
     Bankcode INTEGER NOT NULL,
     PRIMARY KEY (BranchCode),
-    FOREIGN KEY (Bankcode) REFERENCES Banks(Bankcode)
-
+    FOREIGN KEY (Bankcode) REFERENCES Banks(Bankcode),
+FOREIGN KEY (`ADDRESS`) REFERENCES ADDRESS (ADDRESSID)
 
 );
 
@@ -55,7 +57,7 @@ CREATE TABLE Admins (
 
 CREATE TABLE BANKACCOUNTS (
     AccNo BIGINT NOT NULL,
-    Type VARCHAR(255) ,
+     TYPE ENUM('SAVINGS','CURRENT'),
     BranchCode INTEGER,
     customerId int NOT NULL,
     PRIMARY KEY (AccNo),
@@ -68,8 +70,8 @@ CREATE TABLE IF NOT EXISTS TRANSACTIONS(
     TRANSACTIONNO INT NOT NULL,
     AccNo BIGINT NOT NULL,
     AMOUNT BIGINT ,
-    TYPE ENUM('SAVINGS','CURRENT'),
-    TIMESTAMP DATETIME,
+    TYPE ENUM('DEBIT','CREDIT'),
+    TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (AccNo) REFERENCES BANKACCOUNTS(AccNo),
     PRIMARY KEY (TRANSACTIONNO)
 );
@@ -81,8 +83,24 @@ CREATE TABLE reversalRequest(
     PRIMARY KEY (requestid),
     FOREIGN KEY (transactionNo) REFERENCES TRANSACTIONS(transactionNo)
 );
+CREATE TABLE address(
+  addressId int NOT NULL ,
+  street varchar(50) NOT NULL,
+  city varchar(50) NOT NULL,
+  state varchar(50) NOT NULL,
+  country varchar(50) NOT NULL,
+   pincode int NOT NULL,
+  PRIMARY KEY (addressId)
+);
 
-INSERT INTO Banks VALUES('1234','SBI','HYDERABAD');
-INSERT INTO Banks VALUES('4356','ICICI','BANGALORE');
+INSERT into address VALUES('20','Nizampet','Hyderabad','Telangana','India','500090');
 
+INSERT INTO Banks VALUES('1234','SBI','20');
+INSERT INTO Banks VALUES('4356','ICICI','20');
 
+insert into customers VALUES('12','amruth','20');
+insert into branches VALUES('250','20','patil','Delhi','1234');
+insert into employee VALUES('430','sridevi','250');
+
+insert into bankaccounts values('89023383','savings','250','12');
+INSERT INTO TRANSACTIONS VALUES('2450','89023383','1000','debit',default);
